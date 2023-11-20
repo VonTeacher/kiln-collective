1737ccdd684861e4d4e92020d0a1301cd09b3706
Vaughn Weiss <vaughn.weiss@gmail.com>
2023-11-20 15:03:43 -0700

    Extract TitleBar

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React, { useState } from "react";
-import MobileMenu from "./mobileMenu";
-import CloseIcon from "@mui/icons-material/Close";
-import MenuIcon from "@mui/icons-material/Menu";
+
+import MobileMenu from "./MobileMenu";
+import TitleBar from "./TitleBar";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,12 +12,7 @@ const Header = () => {
   return (
     <header className="container flex flex-col border-b border-black">
       <MobileMenu isOpen={isMenuOpen} />
-      <div className="order-1 flex flex-row items-center justify-between p-2 text-center">
-        <div className="flex-grow text-4xl">Kiln Collective</div>
-        <div className="mr-2 sm:hidden" onClick={toggleMenu}>
-          {isMenuOpen ? <CloseIcon /> : <MenuIcon />}
-        </div>
-      </div>
+      <TitleBar isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
     </header>
   );
 };
